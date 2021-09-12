1431ae982e74d0fcf8bd204354b37fd08af46bcb
Yansel <yanselfgok@gmail.com>
2021-09-11 22:38:21 -0400

    Merge branch main into develop

--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,8 @@ import { PublicKey } from "@solana/web3.js";
  */
  export class Constants {
 
-    static MSP_PROGRAM = new PublicKey('H6wJxgkcc93yeUFnsZHgor3Q3pSWgGpEysfqKrwLtMko');
-    static MSP_OPS = new PublicKey('CLazQV1BhSrxfgRHko4sC8GYBU3DoHcX4xxRZd12Kohr');
+    static MSP_PROGRAM = new PublicKey('9yMq7x4LstWYWi14pr8BEBsEX33L3HnugpiM2PT96x4k');
+    static MSP_OPS = new PublicKey('BgxJuujLZDR27SS41kYZhsHkXx6CP2ELaVyg1qBxWYNU');
     static MEMO_PROGRAM = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
     static ASSOCIATED_TOKEN_PROGRAM = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
     static WSOL_TOKEN_MINT = new PublicKey('So11111111111111111111111111111111111111112');
